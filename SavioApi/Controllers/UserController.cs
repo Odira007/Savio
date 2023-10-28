49106c317423c9b8de7ac6d907f391d80425900a
dabirideji <115591934+dabirideji@users.noreply.github.com>
2023-10-28 21:50:21 +0100

    complpeted user endpoints

--- SavioApi/Controllers/UserController.cs
+++ SavioApi/Controllers/UserController.cs
@@ -1,88 +1,170 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
+using CodeCommApi.Dependencies;
+using CodeCommApi.Response;
 using Microsoft.AspNetCore.Mvc;
 using SavioApi.Dependencies.Interfaces;
 using SavioApi.Dto.User.Requests;
+using SavioApi.Dto.User.Response;
 using SavioApi.Models;
 
 namespace SavioApi.Controllers
 {
-    [Route("api/users")]
+    [Route("api/Users")]
     [ApiController]
     public class UserController : ControllerBase
     {
-        private readonly IUserService _userService;
+        private readonly IUserService _service;
+        private readonly IMapper _mapper;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService service, IMapper mapper)
         {
-            _userService = userService;
+            _service = service;
+            _mapper = mapper;
         }
 
-        [HttpPost("create")]
-        public async Task<ActionResult<User>> CreateUser([FromBody] CreateUserDto dto)
+        [HttpPost("CreateUser")]
+        public async Task<ActionResult<DefaultResponse<ReadUserDto>>> CreateUser(
+            [FromBody] CreateUserDto dto
+        )
         {
-            var user = await _userService.CreateUser(dto);
-            if (user == null)
+            var x = new AutoResponse<ReadUserDto>();
+            try
             {
-                return BadRequest("User creation failed or user already exists.");
+                var user = await _service.CreateUser(dto);
+                if (user == null)
+                {
+                    return BadRequest(x.ConvertToBad("UNABLE TO CREATE USER"));
+                }
+                var userDto = _mapper.Map<ReadUserDto>(user);
+                var response = x.ConvertToGood("USER CREATED SUCCESSFULLY");
+                response.Data = userDto;
+
+                return Ok(response);
             }
-            return Ok(user);
+            catch (Exception ex)
+            {
+                return StatusCode(500, x.ConvertToBad($"{ex.Message}"));
+            }
         }
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteUser(Guid id)
+        [HttpDelete("DeleteUser/{id}")]
+        public async Task<ActionResult<DefaultResponse<ReadUserDto>>> DeleteUser(Guid id)
         {
-            var result = await _userService.DeleteUser(id);
-            if (!result)
+            var x = new AutoResponse<ReadUserDto>();
+            try
             {
-                return NotFound("User not found or deletion failed.");
+                var result = await _service.DeleteUser(id);
+                if (!result)
+                {
+                    return BadRequest(x.ConvertToBad("USER NOT FOUND OR DELETION FAILED"));
+                }
+
+                return StatusCode(204, x.ConvertToGood("USER DELETED SUCCESSFULLY"));
             }
-            return NoContent();
+            catch (Exception ex)
+            {
+                return StatusCode(500, x.ConvertToBad($"{ex.Message}"));
+            }
         }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUserById(Guid id)
+        [HttpGet("GetUserById/{UserId}")]
+        public async Task<ActionResult<DefaultResponse<ReadUserDto>>> GetUserById(
+            [FromRoute] Guid UserId
+        )
         {
-            var user = await _userService.FindUser(id);
-            if (user == null)
+            var x = new AutoResponse<ReadUserDto>();
+            try
             {
-                return NotFound("User not found.");
+                var user = await _service.FindUser(UserId);
+                if (user == null)
+                {
+                    return NotFound(x.ConvertToBad("USER NOT FOUND"));
+                }
+                var userDto = _mapper.Map<ReadUserDto>(user);
+                var response = x.ConvertToGood("USER FOUND SUCCESSFULLY");
+                response.Data = userDto;
+                return Ok(response);
             }
-            return Ok(user);
+            catch (Exception ex)
+            {
+                return StatusCode(500, x.ConvertToBad($"{ex.Message}"));
+            }
         }
 
-        [HttpGet]
-        public async Task<ActionResult<List<User>>> GetUsers()
+        [HttpGet("GetAllUsers")]
+        public async Task<ActionResult<DefaultResponse<List<ReadUserDto>>>> GetUsers()
         {
-            var users = await _userService.GetUsers();
-            if (users == null || users.Count == 0)
+            var x = new AutoResponse<List<ReadUserDto>>();
+
+            try
             {
-                return NotFound("No users found.");
+                var users = await _service.GetUsers();
+                if (users == null || users.Count == 0)
+                {
+                    return NotFound(x.ConvertToBad("USERS NOT FOUND"));
+                }
+                var usersDto = users.Select(x => _mapper.Map<ReadUserDto>(x)).ToList();
+                var response = x.ConvertToGood("USERS FOUND SUCCESSFULLY");
+                response.Data = usersDto;
+                return Ok(response);
             }
-            return Ok(users);
+            catch (Exception ex)
+            {
+                return StatusCode(500, x.ConvertToBad($"{ex.Message}"));
+            }
         }
 
-        [HttpPut("{id}")]
-        public async Task<ActionResult<User>> UpdateUser(Guid id, [FromBody] UpdateUserDto dto)
+        [HttpPut("UpdateUser/{USerId}")]
+        public async Task<ActionResult<DefaultResponse<ReadUserDto>>> UpdateUser(
+            [FromRoute] Guid UserId,
+            [FromBody] UpdateUserDto dto
+        )
         {
-            var user = await _userService.UpdateUser(id, dto);
-            if (user == null)
+            var x = new AutoResponse<ReadUserDto>();
+
+            try
             {
-                return NotFound("User not found or update failed.");
+                var user = await _service.UpdateUser(UserId, dto);
+                if (user == null)
+                {
+                    return BadRequest(x.ConvertToBad("UPDATE FAILED, USER NOT FOUND"));
+                }
+                var userDto = _mapper.Map<ReadUserDto>(user);
+                var response = x.ConvertToGood("USER UPDATED SUCCESSUFULLY");
+                response.Data = userDto;
+                return Ok(response);
             }
-            return Ok(user);
+            catch (Exception ex)
+            {
+                return StatusCode(500, x.ConvertToBad($"{ex.Message}"));
+            }
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<User>> UserLogin([FromBody] UserLoginDto dto)
+        [HttpPost("UserLogin")]
+        public async Task<ActionResult<DefaultResponse<ReadUserDto>>> UserLogin(
+            [FromBody] UserLoginDto dto
+        )
         {
-            var user = await _userService.UserLogin(dto);
-            if (user == null)
+            var x = new AutoResponse<ReadUserDto>();
+            try
             {
-                return Unauthorized("Invalid credentials or user not found.");
+                var user = await _service.UserLogin(dto);
+                if (user == null)
+                {
+                    return Unauthorized(x.ConvertToBad("LOGIN FAILED"));
+                }
+                var userDto = _mapper.Map<ReadUserDto>(user);
+                var response = x.ConvertToGood("LOGIN SUCCESSFULL");
+                response.Data = userDto;
+                return Ok(response);
             }
-            return Ok(user);
+            catch (Exception ex)
+            {
+                return StatusCode(500, x.ConvertToBad($"{ex.Message}"));
+            }
         }
     }
 }
