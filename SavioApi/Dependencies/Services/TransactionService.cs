88c9ff58bbde6424378691168d3e2f0249d2b0fc
dabirideji <115591934+dabirideji@users.noreply.github.com>
2023-10-29 15:54:36 +0100

    fixed a bug
    worked on the error to chceck if the sender accountID and receiver account number are valid in a transaction

--- SavioApi/Dependencies/Services/TransactionService.cs
+++ SavioApi/Dependencies/Services/TransactionService.cs
@@ -16,54 +16,69 @@ using SavioApi.Response;
 
 namespace SavioApi.Dependencies.Services
 {
-    public class TransactionService:ITransactionService
+    public class TransactionService : ITransactionService
     {
-         private readonly SavioDbContext _context; 
+        private readonly SavioDbContext _context;
         private readonly IMapper _mapper;
 
         private IUserService _user;
         private IAccountService _account;
-        AutoTransactionResponse<Transaction> x=new();
+        AutoTransactionResponse<Transaction> x = new();
 
-        public TransactionService(SavioDbContext context,IAccountService account,IMapper mapper,IUserService user)
+        public TransactionService(
+            SavioDbContext context,
+            IAccountService account,
+            IMapper mapper,
+            IUserService user
+        )
         {
             _context = context;
-            _account=account;
-            _user=user;
-            _mapper=mapper;
+            _account = account;
+            _user = user;
+            _mapper = mapper;
         }
-          public async Task<TransactionResponse<Transaction>> SendMoney(CreateTransactionDto dto)
+
+        public async Task<TransactionResponse<Transaction>> SendMoney(CreateTransactionDto dto)
         {
-            
-            var transaction=_mapper.Map<Transaction>(dto);
-            GetAccountDto accountDto=new(){
-            AccountNumber=dto.TransactionReceiver,
-            BankName=dto.BankName};
-            var receivingAccount=await _account.GetAccountByAccountNumber(accountDto);
-            var sendingaccount=await _account.GetAccountByAccountId(dto.AccountId);
+            var transaction = _mapper.Map<Transaction>(dto);
+            GetAccountDto accountDto =
+                new() { AccountNumber = dto.TransactionReceiver, BankName = dto.BankName };
+            var receivingAccount = await _account.GetAccountByAccountNumber(accountDto);
+            var sendingaccount = await _account.GetAccountByAccountId(dto.AccountId);
             // var receivingAccount=await _account.GetAccountByAccountId(dto.TransactionReceiver);
-            if(sendingaccount==null||receivingAccount==null){
-                return x.Failed($"INVALID TRANSACTION : INCORRECT USER DETAILS || Sender: {sendingaccount} | Reciever: {receivingAccount}");
+            if (sendingaccount == null || receivingAccount == null)
+            {
+                if (receivingAccount == null)
+                {
+                    return x.Failed("RECEIVING ACCOUNT NOT FOUND");
+                }
+                var senderErrorInfo =
+                    (sendingaccount != null) ? sendingaccount.AccountId : dto.AccountId;
+
+                return x.Failed(
+                    $"INVALID TRANSACTION : INCORRECT USER DETAILS || Sender: {senderErrorInfo} | Reciever: {receivingAccount.AccountId}"
+                );
             }
-           if(receivingAccount==null){
-            return x.Failed("RECEIVING ACCOUNT NOT FOUND");
-           }
-           var balance=sendingaccount.AccountBalance;
-           if(balance<dto.TransactionAmount){
-            return x.Declined("INSUFFICIENT ACCOUNT BALANCE");
-           }
-           await _account.UpdateAccountBalance(dto.AccountId,balance-dto.TransactionAmount);
-           await _account.UpdateAccountBalance(receivingAccount.AccountId,receivingAccount.AccountBalance+dto.TransactionAmount);
-           transaction.Account=sendingaccount;
-           transaction.TransactionTime=DateTime.Now;
-           transaction.ReceivingAccount=receivingAccount.AccountId;
-           transaction.TransactionType=TransactionType.Debit;
-           var response=x.Approved("TRANSFER SUCCESSFUL");
-           response.Data=transaction;
+
+            var balance = sendingaccount.AccountBalance;
+            if (balance < dto.TransactionAmount)
+            {
+                return x.Declined("INSUFFICIENT ACCOUNT BALANCE");
+            }
+            await _account.UpdateAccountBalance(dto.AccountId, balance - dto.TransactionAmount);
+            await _account.UpdateAccountBalance(
+                receivingAccount.AccountId,
+                receivingAccount.AccountBalance + dto.TransactionAmount
+            );
+            transaction.Account = sendingaccount;
+            transaction.TransactionTime = DateTime.Now;
+            transaction.ReceivingAccount = receivingAccount.AccountId;
+            transaction.TransactionType = TransactionType.Debit;
+            var response = x.Approved("TRANSFER SUCCESSFUL");
+            response.Data = transaction;
             await _context.Transactions.AddAsync(transaction);
             await _context.SaveChangesAsync();
             return response;
-            
         }
 
         public Task<TransactionResponse<Transaction>> ApproveTransaction(Guid TransactionId)
@@ -99,9 +114,10 @@ namespace SavioApi.Dependencies.Services
             throw new NotImplementedException();
         }
 
-      
-
-        public Task<TransactionResponse<Transaction>> UpdateTransaction(Guid TransactionId, UpdateTransactionDto dto)
+        public Task<TransactionResponse<Transaction>> UpdateTransaction(
+            Guid TransactionId,
+            UpdateTransactionDto dto
+        )
         {
             throw new NotImplementedException();
         }
