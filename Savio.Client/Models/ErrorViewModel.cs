e55993a8c8a2bfe68de1db2a58be776e750cf33a
Odira Ikewelugo <odiraikewelugo@gmail.com>
2023-10-30 09:52:50 +0100

    Initial commit

---
+++ Savio.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Savio.Client.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
